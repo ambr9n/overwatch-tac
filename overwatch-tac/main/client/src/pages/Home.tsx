1bb4552fe7299873e863b27f4e0c2af784fbff89
owen hanson <ambrynhanson@gmail.com>
2026-03-03 13:01:49 -0500

    update comments for home page

--- overwatch-tac/main/client/src/pages/Home.tsx
+++ overwatch-tac/main/client/src/pages/Home.tsx
@@ -1,17 +1,23 @@
 const Home = () => {
   return (
+    
     <div style={{ padding: "40px", textAlign: "center" }}>
       <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>
-        Overwatch Tactical Map
+        {/* HEADER */}
+        Overwatch Tac Map [temp] {/* we can choose aa proper name in the future before final, delete comment before final */}
       </h1>
 
       <p style={{ fontSize: "18px", color: "#ccc" }}>
-        Plan team comps. Draw strategies. Coordinate ult rotations.
+        {/* SUBHEADER */}
+        {/* TEMPORARY, must change the temporary name, and the caption, delete this cimment before review, delete comment before final */}
+        overwatch-tac-temp is a tool built for Overwatch 2 players to strategize, coordinate, and develop plans to win! 
       </p>
 
       <div style={{ marginTop: "40px" }}>
+        {/* BUTTON POINTING TO TAC MAP PAGE */}
+        {/* href IS THE POINTER, delete before final */}
         <a
-          href="/tacmap"
+          href="/tacmap" 
           style={{
             padding: "15px 30px",
             background: "orange",
@@ -21,6 +27,8 @@ const Home = () => {
             fontWeight: "bold",
           }}
         >
+        {/* LABEL IN BUTTON */}
+        {/* POTENTIALLY */}
           Start Planning
         </a>
       </div>
